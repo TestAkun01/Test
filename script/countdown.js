7f4cf90d547182bd7bd3fd9a471a1e60560c9c39
TestAkun01 <160554576+TestAkun01@users.noreply.github.com>
2024-05-01 15:10:22 +0000

    ganti jam

--- script/countdown.js
+++ script/countdown.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Apr 22, 2024 20:28:00").getTime();
+var countDownDate = new Date("May 11, 2024 9:00:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
